871275b0a0f116b983e4a0433d018824b22c14f6
Darrick Shen <Dshen821@ucla.edu>
2018-01-25 07:19:35 +0000

    adjustment to autoSim.R to create n=100,200,300,400,500 for all 3 distributions, written into text files. text files appear to be empty.

--- autoSim.R
+++ autoSim.R
@@ -3,15 +3,17 @@
 #Modify the autoSim.R script to run simulations with combinations of sample sizes
 #nVals = seq(100, 500, by=100) and distributions  distTypes = c("gaussian", "t1", "t5")
 #and write output to appropriately named files. Use rep = 50, and seed = 280
-
-
+rep = 50
+seednum=280
 nVals = seq(100, 500, by=100)
-for (n in nVals) {
-  oFile = paste("n", n, ".txt", sep="")
-  arg = paste("n=", n, sep="")
-  sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
-  system(sysCall)
-  print(paste("sysCall=", sysCall, sep=""))
+distTypes = c("gaussian", "t1", "t5")
+for (i in distTypes){
+  for (n in nVals) {
+    oFile = paste(distTypes,"_n_", n, ".txt", sep="")
+    arg = paste("seed=",seednum," n=",n," dist=",distTypes, " rep=",rep, " n=",n, sep="")
+    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
+    system(sysCall)
+    print(paste("sysCall=", sysCall, sep=""))
+  }
 }
-
 
