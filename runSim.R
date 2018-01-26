b52052c1b2d63edc8357c83bebae5da48c1873a9
Darrick Shen <Dshen821@ucla.edu>
2018-01-26 01:19:20 +0000

    -added n arguments to t distributions -added a call to the MSE function -deleted comments on the problem instructions

--- runSim.R
+++ runSim.R
@@ -31,26 +31,17 @@ for (i in 1:rep){
       x = rnorm(n)
     }
   else if (dist == "t1"){
-      x = rt(1)
+      x = rt(n,1)
     }
   else if (dist == "t5"){
-      x = rt(5)
+      x = rt(n,5)
   }
   classicalmean[i]<-mean(x)
-  
   meanPrimes[i]=estMeanPrimes(x)
 }
  # return(classicalmean)
   classicalMSE = sum((classicalmean)^2)/rep
   primeMSE = sum((meanPrimes)^2)/rep
   return(c(classicalMSE,primeMSE))
-  
 }
-
-#Expand the runSim.R script to include arguments seed (random seed), n (sample size), 
-#dist (distribution) and rep (number of simulation replicates). When dist="gaussian", 
-#generate data from standard normal; when dist="t1", generate data from t-distribution 
-#with degree of freedom 1 (same as Cauchy distribution); when dist="t5", generate data 
-#from t-distribution with degree of freedom 5. Calling runSim.R will (1) set random seed
-#according to argument seed, (2) generate data according to argument dist, (3) compute the
-#primed-indexed average estimator in class and the classical sample average estimator for each simulation replicate, (4) report the average mean squared error (MSE)
+MSE(seed,n,dist,rep)
