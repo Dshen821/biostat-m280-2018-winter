9bf23f34dc50a1f07b1763929c0f1a623d22c533
Darrick Shen <Dshen821@ucla.edu>
2018-01-26 02:11:24 +0000

    For loops was adjusted for distributions gaussian, t1, t5 to run through values 100,200,300,400,500 for sample size. (instead of vice-versa)
    arg was redefined as the paste of three different pasted strings, because in order for the distribution argument to be read in correctly in linux, there must be a backslash preceding each single quotation. However, these are not used in R.
    
    proper format: 'dist=\"gaussian\"'

--- autoSim.R
+++ autoSim.R
@@ -6,11 +6,11 @@
 rep = 50
 seednum=280
 nVals = seq(100, 500, by=100)
-distTypes = c("'gaussian'", "'t1'", "'t5'")
-for (i in distTypes){
-  for (n in nVals) {
-    oFile = paste(distTypes,"_n_", n, ".txt", sep="")
-    arg = paste("seed=",seednum," n=",n," dist=",distTypes, " rep=",rep, " n=",n, sep="")
+distributions = c("gaussian", "t1", "t5")
+for (n in nVals){
+  for (dist in distributions) {
+    oFile = paste(dist,"n", n, ".txt", sep="_")
+    arg = paste(paste("seed=",seed," n=",n,sep = ""),paste("\'dist=",'\"',dist,'\"',"\'",sep = ""), paste(" rep=",rep, " n=",n, sep=""),sep = " ")
     sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
     system(sysCall)
     print(paste("sysCall=", sysCall, sep=""))
