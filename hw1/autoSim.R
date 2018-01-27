bdafeca4b57f572bca3804b08481d0dc2848a716
Dshen821 <35546757+Dshen821@users.noreply.github.com>
2018-01-27 01:45:21 -0800

    Reupload of R scripts (completed)
    Reupload

---
+++ hw1/autoSim.R
@@ -0,0 +1,19 @@
+# autoSim.R
+
+#Modify the autoSim.R script to run simulations with combinations of sample sizes
+#nVals = seq(100, 500, by=100) and distributions  distTypes = c("gaussian", "t1", "t5")
+#and write output to appropriately named files. Use rep = 50, and seed = 280
+rep = 50
+seed=280
+nVals = seq(100, 500, by=100)
+distributions = c("gaussian", "t1", "t5")
+for (n in nVals){
+  for (dist in distributions) {
+    oFile = paste(dist,"n", n, ".txt", sep="_")
+    arg = paste(paste("seed=",seed," n=",n,sep = ""),paste("\'dist=",'\"',dist,'\"',"\'",sep = ""), paste(" rep=",rep, " n=",n, sep=""),sep = " ")
+    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
+    system(sysCall)
+    print(paste("sysCall=", sysCall, sep=""))
+  }
+}
+
---
+++ hw1/tablecons.R
@@ -0,0 +1,14 @@
+dat <- lapply(Sys.glob("*_.txt"), read.table)
+
+unl_dat <- matrix(unlist(dat), byrow = TRUE, ncol = 3)[, -1]; 
+unl_dat2 <- unl_dat[c(6:15, 1:5), c(2, 1)]; 
+unl_dat2_trans <- c(t(unl_dat2)) #transpose
+matdat <- matrix(unl_dat2_trans, nrow = 10)
+
+datfrmdat <- as.data.frame(matdat)
+avgNames <- rep(c("Prime", "Class"), times = 5)
+nObs <- rep(c(100, 200, 300, 400, 500), each = 2)
+labeled_datfrmdat <- cbind(nObs, avgNames, datfrmdat)
+colnames(labeled_datfrmdat) <- c("n", "Method", "t1", "t5", "Gaussian")
+library(knitr)
+kable(labeled_datfrmdat)
