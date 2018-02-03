ce7aee14d3f2347e80fb856938b1bf2f0de5a3ec
Darrick Shen <Dshen821@ucla.edu>
2018-02-03 00:17:21 +0000

    Better Spacing. Semicolons to periods in comments Adjusted comment placement.
    Last problem I decided to call tableCons.R with bash code instead of R code.
    
    Added column name allignment to be left alligned.

--- hw1/runSim.R
+++ hw1/runSim.R
@@ -1,10 +1,9 @@
+  # Parsing command arguments
 for (arg in commandArgs(TRUE)) {
-  # parsing command arguments
   eval(parse(text = arg))
 }
-
+  # Check if a given integer is prime
 isPrime = function(n) {
-  # check if a given integer is prime
   if ( n <= 3 ) {
     return (TRUE)
   }
@@ -13,17 +12,14 @@ isPrime = function(n) {
   }
   return (TRUE)
 }
-
+  # Estimate mean only using observation with prime indices.
 estMeanPrimes = function (x) {
-  # estimate mean only using observation with prime indices
   n = length(x)
   ind = sapply(1:n, isPrime)
   return (mean(x[ind]))
 }
-
-MSE = function (seed,n,dist,rep){
   # Mean Squared Error Calculation for classical and prime mean.
-  #
+MSE = function (seed, n, dist, rep){
   # Args: 
   #   seed=seed number.
   #   n = sample size.
@@ -34,19 +30,17 @@ MSE = function (seed,n,dist,rep){
   set.seed(seed)
   for (i in 1:rep){
     if (dist == "gaussian"){
-      x = rnorm(n)
-    }
-    else if (dist == "t1"){
-      x = rt(n,1)
-    }
-    else if (dist == "t5"){
-      x = rt(n,5)
+      x <- rnorm(n)
+    } else if (dist == "t1") {
+      x <- rt(n, 1)
+    } else if (dist == "t5") {
+      x <- rt(n, 5)
     }
     classicalmean[i] <- mean(x)  # returns classical mean.
-    meanPrimes[i] <- estMeanPrimes(x) # returns prime mean.
+    meanPrimes[i] <- estMeanPrimes(x)  # returns prime mean.
   }
   classicalMSE = sum((classicalmean)^2)/rep # returns classical MSE.
-  primeMSE = sum((meanPrimes)^2)/rep # returns prime MSE.
+  primeMSE = sum((meanPrimes)^2)/rep  # returns prime MSE.
   return(c(classicalMSE, primeMSE))
 }
 MSE(seed, n, dist, rep)
